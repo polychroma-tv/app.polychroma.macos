b4e0bf7c96b978521f5d81b274950ffb1be2fe8f
Polychroma, LLC <support@polychroma.co>
2017-10-11 17:59:29 -0400

    Add files via upload

---
+++ v1.1/TV Polychroma/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TV Polychroma
+//
+//  Copyright © 2017 – Polychroma, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
